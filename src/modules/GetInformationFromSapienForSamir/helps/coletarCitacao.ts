231ac61bc31d32957f69f78036ee169068c13a13
@patrick.sampaio <patrick.sampaio@agu.gov.br>
2024-06-10 11:15:02 -0300

    corrigindo erro na busca da citqacao padrao, chave date nao encontrada

--- src/modules/GetInformationFromSapienForSamir/helps/coletarCitacao.ts
+++ src/modules/GetInformationFromSapienForSamir/helps/coletarCitacao.ts
@@ -5,7 +5,13 @@ export function coletarCitacao(arrayDeDocumentos: ResponseArvoreDeDocumento[]): 
     if (ObjectCitacao == null) {
         return null
     }
-    const ArrayDataCitacaoParaFormatacao = ObjectCitacao.documentoJuntado.dataHoraProducao.date.split(" ")[0].split("-");
-    const dataCitacao: string = `${ArrayDataCitacaoParaFormatacao[2]}/${ArrayDataCitacaoParaFormatacao[1]}/${ArrayDataCitacaoParaFormatacao[0]}`
-    return dataCitacao;
+    
+    try {
+        const ArrayDataCitacaoParaFormatacao = ObjectCitacao.documentoJuntado.dataHoraProducao.date.split(" ")[0].split("-");
+        const dataCitacao: string = `${ArrayDataCitacaoParaFormatacao[2]}/${ArrayDataCitacaoParaFormatacao[1]}/${ArrayDataCitacaoParaFormatacao[0]}`
+        return dataCitacao;
+    } catch (error) {
+        return null
+    }
 }
+
